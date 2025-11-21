1e0d22729a02ea5a99256d359e9154b76bf063c4
Mirko Pavlinović <MirkoP@teched.hr>
2025-11-21 15:36:43 +0100

    redesigned datatables

--- Review/obj/Debug/net8.0/Review.AssemblyInfo.cs
+++ Review/obj/Debug/net8.0/Review.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Review")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4bf8dffd040a905e5018c50cf73705fc1eb99f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfe0e2f548c1d3bb1793d792089ce21f5fdd77e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Review")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Review")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
